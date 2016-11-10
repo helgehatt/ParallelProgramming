4be6a0b7334e8faf171612dd3659bb92b6e9934d
Thomas58 <dktdn@hotmail.com>
2016-11-10 16:58:58 +0100

    Tests

--- src/CarTest.java
+++ src/CarTest.java
@@ -24,7 +24,46 @@ public class CarTest extends Thread {
                 sleep(3000);
                 cars.stopAll();
                 break;
-
+            case 1:
+            	cars.println("Going to mach-speed!");
+            	for (int i = 1; i < 9; i++) {
+                    cars.setSpeed(i,10);
+                    cars.setVariation(i, 0);
+                };
+                cars.startAll();
+            	break;
+            case 2:
+            	cars.println("High variation");
+            	for (int i = 1; i < 9; i++) {
+                    cars.setVariation(i, 20);
+                };
+                cars.startAll();
+            	break;
+            case 3:
+            	cars.println("Barrier on-off");
+            	cars.barrierOn();
+            	cars.startAll();
+            	for (int i = 0; i < 100; i++){
+            		cars.barrierOff();
+            		cars.barrierOn();
+            		Thread.sleep(100);
+            	}
+            	cars.stopAll();
+            	break;
+            case 4:
+            	cars.println("Barrier threshold");
+            	cars.barrierOn();
+            	cars.startAll();
+            	cars.startBarrierSet(4);
+            	cars.startBarrierSet(9);
+            	break;
+            case 5:
+            	cars.println("Remove and restore car");
+            	for (int i = 0; i < 10; i++){
+            		cars.removeCar(1);
+                	cars.restoreCar(1);
+            	}
+            	break;
             case 19:
                 // Demonstration of speed setting.
                 // Change speed to double of default values
